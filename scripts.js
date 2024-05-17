fc53c7b018d50657d557206ec0084ac4ffcf274b
Your Name <you@example.com>
2024-05-17 13:18:58 +0530

    hi

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Simple script for smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
